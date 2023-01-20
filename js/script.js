19c9153bbaa272202fc8eb615aa0d185eaca1c38
Barbara Komenda <122162134+BarbaraKomenda@users.noreply.github.com>
2023-01-20 16:40:42 +0100

    changing color

--- js/script.js
+++ js/script.js
@@ -45,11 +45,11 @@
                 <button class="tasks__button tasks__button--toggleDone js-toggleDone">
                     ${task.done ? "&#10003;" : ""}
                 </button>
-                <span class="tasks__content${task.done ? " tasks__content--done" : ""}">${task.content}</span>
+                <span class="tasks__content${task.done ? " tasks__content--done" : ""}">${task.content}
+                </span>
                 <button class="tasks__button tasks__button--remove js-remove">
                 🗑
                 </button>
-        
             </li>
            `;
         }
