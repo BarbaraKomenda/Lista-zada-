c30dc1aeaf17cdd88b14116290512f1a588195fd
Barbara Komenda <122162134+BarbaraKomenda@users.noreply.github.com>
2023-01-20 14:58:40 +0100

    correcting js

--- js/script.js
+++ js/script.js
@@ -5,19 +5,19 @@
         tasks.splice(taskIndex, 1);
         render();
     };
-    
+
     const toggleTaskDone = (taskIndex) => {
         tasks[taskIndex].done = !tasks[taskIndex].done;
         render();
     };
 
     const addNewTask = (newTaskContent) => {
-        tasks.push({ content: newTaskContent});
+        tasks.push({ content: newTaskContent });
         render()
     };
 
     const bindRemoveEvents = () => {
-        const removeButtons = document.querySelectorAll("js-remove");
+        const removeButtons = document.querySelectorAll(".js-remove");
 
         removeButtons.forEach((removeButton, taskIndex) => {
             removeButton.addEventListener("click", () => {
@@ -27,7 +27,7 @@
     };
 
     const bindToggleDoneEvents = () => {
-        const toggleDoneButtons = document.querySelectorAll("js-toggleDone");
+        const toggleDoneButtons = document.querySelectorAll(".js-toggleDone");
 
         toggleDoneButtons.forEach((toggleDoneButton, taskIndex) => {
             toggleDoneButton.addEventListener("click", () => {
@@ -40,18 +40,18 @@
         let tasksList = "";
 
         for (const task of tasks) {
-            tasksList +=`
-            <li class="tasks__item ja-task">
-                <button class="tasks__button tasks__buton--toggleDeon js-toggleDone">
+            tasksList += `
+            <li class="tasks__item js-task">
+                <button class="tasks__button tasks__button--toggleDone js-toggleDone">
                     ${task.done ? "&#10003;" : ""}
                 </button>
-                <span class="tasks__content${ task.done ? " tasks__content--done" : ""}">${task.content}</span>
-                <button class="tasks__button tasks__button--remove ja-remove">
+                <span class="tasks__content${task.done ? " tasks__content--done" : ""}">${task.content}</span>
+                <button class="tasks__button tasks__button--remove js-remove">
                     fa fa-trash-o
                 </button>
         
             </li>
-           `; 
+           `;
         }
         document.querySelector(".js-tasks").innerHTML = tasksList;
 
@@ -62,23 +62,23 @@
     const onFormSubmit = (event) => {
         event.preventDefault();
 
-        const newTaskElement = document.querySelector("js-newTask");
-        const newTaskContent = newTaskElement.ariaValueMax.trim();
+        const newTaskElement = document.querySelector(".js-newTask");
+        const newTaskContent = newTaskElement.value.trim();
 
-        if (newTaskContent !=="") {
+        if (newTaskContent !== "") {
             addNewTask(newTaskContent);
             newTaskElement.value = "";
-}
-newTaskElement.focus();
+        }
+        newTaskElement.focus();
     };
 
     const init = () => {
-render();
+        render();
 
-const form = document.querySelector("js-form");
-form.addEventListener("submit", onFormSubmit);
-};
+        const form = document.querySelector(".js-form");
+        form.addEventListener("submit", onFormSubmit);
+    };
 
-init();
+    init();
 
 }
